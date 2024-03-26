f68b1b37e0f6a01afe7a9274daf432ecff5fbc7d
sreedhar76 <143782954+sreedhar76@users.noreply.github.com>
2024-03-26 09:21:30 +0530

    Add files via upload

---
+++ integer and float.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int integerNumber = 10;
+    std::cout << "Integer number: " << integerNumber << std::endl;
+
+    float floatNumber = 3.14f;
+    std::cout << "Float number: " << floatNumber << std::endl;
+
+
+    char charValue = 'A';
+    std::cout << "Character value: " << charValue << std::endl;
+
+    return 0;
+}
+
